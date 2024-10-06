46337d73411e1bda0329cfe3d4de492f4849af26
Esk1z0 <j.esteban.rincon.m@gmail.com>
2024-10-06 17:55:55 +0200

    added a timer, modifying reward_runner for better performance

---
+++ drone_tfg_juanes/enviroments_package/Env_Reward_package/reward_dir/reward_timer.py
@@ -0,0 +1,35 @@
+from .reward_basic import RewardStrategyInterface
+import numpy as np
+
+
+class RewardTimer(RewardStrategyInterface):
+
+    @staticmethod
+    def class_name():
+        return "timer"
+
+    def __init__(self, max_time=1):
+        self.max_time = max_time
+        self.max_reward = 1
+        self.start_time = 0
+
+
+    def __str__(self):
+        string = "name: Timer" \
+                 "\ndescription: It sets the maximum time the drone can take to reach its goal"
+        return string
+
+    def start_test(self, obs: dict, time) -> None:
+        self.start_time = time
+
+    def get_reward(self, obs: dict, time) -> (int, bool, bool):
+        reward, terminated, finish = self.max_reward, False, True
+        if self._timeout(time):
+            reward, terminated, finish = -1, True, False
+        return reward, terminated, finish
+
+    def teardown(self):
+        pass
+
+    def _timeout(self, time):
+        return self.max_time < (time - self.start_time)
--- drone_tfg_juanes/enviroments_package/Env_Reward_package/rewards.py
+++ drone_tfg_juanes/enviroments_package/Env_Reward_package/rewards.py
@@ -2,10 +2,12 @@ from .reward_dir.reward_basic import RewardStrategyInterface
 from .reward_dir.reward_no_roll import RewardNoRoll
 from .reward_dir.reward_zone import RewardZone
 from .reward_dir.reward_reach_height import RewardReachHeight
+from .reward_dir.reward_timer import RewardTimer
 
 reward_dict = {
     str(RewardStrategyInterface.class_name()): RewardStrategyInterface,
     str(RewardNoRoll.class_name()): RewardNoRoll,
     str(RewardZone.class_name()): RewardZone,
-    str(RewardReachHeight.class_name()): RewardReachHeight
+    str(RewardReachHeight.class_name()): RewardReachHeight,
+    str(RewardTimer.class_name()): RewardTimer
 }
