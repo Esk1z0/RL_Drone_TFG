00b5e7f94f7bdcbd81c119b251d012896e7701c4
Esk1z0 <j.esteban.rincon.m@gmail.com>
2024-10-07 11:30:34 +0200

    trying to dockerize the app

---
+++ drone_tfg_juanes/run_simulation.py
@@ -0,0 +1,16 @@
+from enviroments_package.Drone_Env import DroneEnv
+import numpy as np
+
+world_dir = "./simulation_package/worlds/my_frst_webots_world.wbt"
+env_config_dir = "./configs/reward_package_config/takeoff.json"
+
+if __name__ == "__main__":
+    env = DroneEnv(world_dir, env_config_dir)
+    env.reset()
+    action = np.array([500, 500, 500, 500])
+    for i in range(50):
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(reward)
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
