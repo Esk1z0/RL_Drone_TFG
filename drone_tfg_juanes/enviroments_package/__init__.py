cef4e87ab0f56fe2c506f678dff62ae0f0fff40f
Esk1z0 <j.esteban.rincon.m@gmail.com>
2024-10-29 09:57:30 +0100

    fix error with registry on gymnasium

--- drone_tfg_juanes/enviroments_package/__init__.py
+++ drone_tfg_juanes/enviroments_package/__init__.py
@@ -3,7 +3,7 @@ from enviroments_package.Drone_Env import DroneEnv
 import gymnasium
 
 register(
-    id='drone_tfg_juanes/Drone-v01',
-    entry_point="enviroments_package:Drone_Env",
+    id='drone_tfg_juanes/Drone-v1',
+    entry_point="enviroments_package.Drone_Env:DroneEnv",
     max_episode_steps=300,
 )
